921ce3e7259158c59d3919903bcec5b1f0449e4e
nibafanfan <nibafanfan@gmail.com>
2024-06-12 07:59:11 +0000

    process

--- stages/02_process.py
+++ stages/02_process.py
@@ -1,7 +1,36 @@
+import os
+import json
 import pandas as pd
 
-# read the headings.csv from cache/01_download/headings.csv
 heading_df = pd.read_csv('cache/01_download/headings.csv') # source, heading, type
+root_folder = 'cache/01_download'
+compound_df = heading_df[heading_df['type'] == 'Compound']
 
-# TODO figure out a general way to parse all the 'Compound' .json files and store them in a parquet file
-# Parse annotations.json to get headings
+data_list = []
+
+for index, row in compound_df.iterrows():
+    source = row['source']
+    heading = row['heading']
+    # Construct the expected file path
+    subdir = os.path.join(root_folder, source)
+    file_path = os.path.join(subdir, f"{heading}.json")
+    
+    if os.path.exists(file_path):
+        with open(file_path, 'r', errors='ignore') as f:
+            try:
+                json_data = json.load(f)
+                if 'Annotations' in json_data and 'Annotation' in json_data['Annotations']:
+                    # Append each annotation directly to the data_list
+                    for annotation in json_data['Annotations']['Annotation']:
+                        data_list.append(annotation)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON from file {file_path}: {e}")
+            except UnicodeDecodeError as e:
+                print(f"Unicode decode error for file {file_path}: {e}")
+
+
+df = pd.DataFrame(data_list)
+os.makedirs('brick', exist_ok=True)
+output_file = 'brick/compound.parquet'
+df.to_parquet(output_file)
+print(f"Data compiled and saved to {output_file}")
